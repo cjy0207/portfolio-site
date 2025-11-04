81f5e33a322fe1e589d9021ed40601d6e4e4dd4b
cjy0207 <jiyunjiyun0723@naver.com>
2025-11-04 12:28:06 +0900

    scroll maigic

---
+++ js/main.js
@@ -0,0 +1,16 @@
+//scroll magic 사용법
+// init controller
+const controller = new ScrollMagic.Controller();
+
+const spyEls = document.querySelectorAll("section.scroll-spy");
+
+//콜백함수
+spyEls.forEach(function (spyEl) {
+  // create a scene
+  new ScrollMagic.Scene({
+    triggerElement : spyEl, //감시할 장면 추가 및 옵션지정
+    triggerHook : 0.5 //화면의 50% 지점에서 조여짐 여부 감시(0-1사이 지정)
+  })
+    .setClassToggle(spyEl, 'show') //요소가 화면에 감지되면 show클래스 추가
+    .addTo(controller); //컨트롤러에 장면을 할당(필수)
+});
